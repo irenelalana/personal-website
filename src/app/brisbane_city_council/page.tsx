753b9b9949ab0f3f2860423485adfd7ff4540317
irenelalana <irela13@hotmail.com>
2025-09-03 13:59:51 +1000

    BCC holidays

--- src/app/brisbane_city_council/page.tsx
+++ src/app/brisbane_city_council/page.tsx
@@ -50,23 +50,19 @@ export default function BrisbaneCityCouncil() {
             </form>
 
             <p>
-              <strong>Functional training for teens during school holidays</strong>: increases endurance and enhances fitness, boost mobility & flexibility; improves balance, coordination and agility, and makes a fun environment building teamwork.
+              <strong>Aquafitness Gold (kid with and adult) during school holidays</strong>: unique and heartwarming Aquafitness class designed for grandparents and children to exercise together! We focus on low-impact, joint-friendly movements and games that improve coordination, balance, and overall fitness. Get ready to splash, play, and work out to a fun mix of classic and modern tunes. This class is a fantastic opportunity to create lasting memories while strengthening family bonds and getting fit as a team! All participants should be confident in shallow water.
             </p>
-            <p>Sessions were run at Moorlands Park, in Aucheflower, Brisbane. Next dates TBC.</p>
-            <p>Just fill the following form out to book your spot</p>
-            <form action="https://forms.gle/2sk8tNs5WWCHRHKx9" target="_blank" rel="noreferrer">
+            <p>Sessions were run at Moorlands Park, in Aucheflower, Brisbane. Next dates at 11.00am: Thursdays 25 Sep, 2 Oct, 18 Dec 2025, 15 Jan, 22 Jan, 9 Apr, 16 Apr 2026.</p>
+            <p>Please, note that after filling the form out with preferences, you will be confirmed by email as spots are limited.</p>
+            <form action="https://forms.gle/xWXUSxYNzE5X7D6GA" target="_blank" rel="noreferrer">
               <button className="form-button">Preactivity form</button>
             </form>
 
             <p>
-              <strong>Aquafitness for teens during school holidays</strong>: a
-              variety of cardio, strength, and games tailored to suit teenage
-              fitness levels and needs. Get ready to splash, move, and groove to
-              upbeat music while improving your overall fitness and having a blast
-              with friends. Participants should be confident in shallow water.
+              <strong>Aquafitness for teens during school holidays</strong>: a variety of cardio, strength, and team activities tailored to suit teenager fitness levels and needs. Get ready to splash, move, and groove to upbeat music while improving your overall fitness and having a blast with friends. Participants should be confident in shallow water.
             </p>
-            <p>Sessions are run at Musgrave Park Pool, in West End, Brisbane. Next dates TBC.</p>
-            <p>Please, note that after filling the form out with preferences, you will be confirmed by email.</p>
+            <p>Sessions are run at Musgrave Park Pool, in West End, Brisbane. Next dates 10.00 to 11.00am: Tuesdays 23 Sep, 30 Sep, 16 Dec 2025, 13 Jan, 20 Jan, 7 Apr, 14 Apr 2026. Thursdays 25 Sep, 2 Oct, 18 Dec 2025, 15 Jan, 22 Jan, 9 Apr, 16 Apr 2026</p>
+            <p>Please, note that after filling the form out with preferences, you will be confirmed by email as spots are limited.</p>
             <form action="https://forms.gle/xWXUSxYNzE5X7D6GA" target="_blank" rel="noreferrer">
               <button className="form-button">Preactivity form</button>
             </form>
