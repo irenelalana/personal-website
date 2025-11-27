30a81b0d2dcf073e4fb9af4feaafc1b113d8794a
irenelalana <irela13@hotmail.com>
2025-11-27 21:20:41 +1000

    bcc bookings

--- src/app/brisbane_city_council/page.tsx
+++ src/app/brisbane_city_council/page.tsx
@@ -1,7 +1,13 @@
 // pages/brisbane_city_council.js
+"use client";
 import React from "react";
 
+
+import Script from "next/script";
+
 export default function BrisbaneCityCouncil() {
+
+
   return (
     <section id="services">
       <div className="card_single">
@@ -20,20 +26,28 @@ export default function BrisbaneCityCouncil() {
               <strong>Aquafitness for adults</strong>: experience an excellent,
               low-impact workout in the water. Feel the benefits
               of exercise without the pain. Using minimum impact movements,
-              participants will develop strength, mobility and cardio with fun
-              tunes.
+              participants will develop strength, mobility and cardio with fun tunes.
             </p>
             <p>Sessions are run at Musgrave Park Pool, in West End, Brisbane. Mondays 5.30pm and Fridays 12.30pm (except School Holidays)</p>
-            <p>
-              Please, note that after filling the initial form out, you will have to send an email to the pool, clicking{" "}
-              <a href="mailto:info@musgraveparkaquaticcentre.com.au">here</a>{" "}
-              every week (before Saturday) to book your spot for the following
-              week as the spots are limited.
-            </p>
+            <p><strong>Step 1</strong>. Fill this initial form out if it is your first time attending.</p>
             <form action="https://forms.gle/mNK6n5XKE9Fng2Yn9" target="_blank" rel="noreferrer">
               <button className="form-button">Preactivity form</button>
             </form>
+            <p><strong>Step 2</strong>. To join any of these sessions, please make sure to book your spot in advance through the calendar below, as spaces are limited.</p>
+            <p>If you are unable to attend, please cancel your booking as early as possible so someone else can take your spot.</p>
+            <p>If the session is fully booked, please check back closer to the start time, as spots may open up if participants cancel.</p>
+            <>
+              <div
+                className="calendly-inline-widget"
+                data-url="https://calendly.com/irene-lalana/bcc-aquafitness-musgrave?hide_event_type_details=1&hide_gdpr_banner=1&background_color=e8e1d0&text_color=076873&primary_color=f19601"
+                style={{ minWidth: "320px", height: "50vh" }}
+              />
 
+              <Script
+                src="https://assets.calendly.com/assets/external/widget.js"
+                strategy="lazyOnload"
+              />
+            </>  
             <p>
               <strong>Fitness for adults</strong>: Using minimum equipment,
               participants will develop strength, mobility and cardio with fun
@@ -48,7 +62,7 @@ export default function BrisbaneCityCouncil() {
             <p>
               <strong>Aquafitness Gold´s & Kids (kid with and adult) during school holidays</strong>: unique and heartwarming Aquafitness class designed for grandparents and children to exercise together! We focus on low-impact, joint-friendly movements and games that improve coordination, balance, and overall fitness. Get ready to splash, play, and work out to a fun mix of classic and modern tunes. This class is a fantastic opportunity to create lasting memories while strengthening family bonds and getting fit as a team! All participants should be confident in shallow water.
             </p>
-            <p>Sessions were run at Musgrave Park Pool, in West End, Brisbane. Next dates at 11.00am: Thursdays 25 Sep, 2 Oct, 18 Dec 2025, 15 Jan, 22 Jan, 9 Apr, 16 Apr 2026.</p>
+            <p>Sessions were run at Musgrave Park Pool, in West End, Brisbane. Next dates at 11.00am: Thursdays 25 Sep, 2 Oct, 15 Jan, 22 Jan, 9 Apr, 16 Apr 2026.</p>
             <p>Please, note that after filling the form out with preferences, you will be confirmed by email as spots are limited.</p>
             <form action="https://forms.gle/xWXUSxYNzE5X7D6GA" target="_blank" rel="noreferrer">
               <button className="form-button">Preactivity form</button>
@@ -57,7 +71,7 @@ export default function BrisbaneCityCouncil() {
             <p>
               <strong>Aquafitness for teens during school holidays</strong>: a variety of cardio, strength, and team activities tailored to suit teenager fitness levels and needs. Get ready to splash, move, and groove to upbeat music while improving your overall fitness and having a blast with friends. Participants should be confident in shallow water.
             </p>
-            <p>Sessions are run at Musgrave Park Pool, in West End, Brisbane. Next dates 10.00 to 11.00am: Tuesdays 23 Sep, 30 Sep, 16 Dec 2025, 13 Jan, 20 Jan, 7 Apr, 14 Apr 2026. Thursdays 25 Sep, 2 Oct, 18 Dec 2025, 15 Jan, 22 Jan, 9 Apr, 16 Apr 2026</p>
+            <p>Sessions are run at Musgrave Park Pool, in West End, Brisbane. Next dates 10.00 to 11.00am: Tuesdays 23 Sep, 30 Sep, 13 Jan, 20 Jan, 7 Apr, 14 Apr 2026. Thursdays 25 Sep, 2 Oct, 18 Dec 2025, 15 Jan, 22 Jan, 9 Apr, 16 Apr 2026</p>
             <p>Please, note that after filling the form out with preferences, you will be confirmed by email as spots are limited.</p>
             <form action="https://forms.gle/xWXUSxYNzE5X7D6GA" target="_blank" rel="noreferrer">
               <button className="form-button">Preactivity form</button>
