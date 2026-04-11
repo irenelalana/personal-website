98ba5b691f091ca84bd8a740d4f0524f7bd1f10a
JairoAussie <50427282+JairoAussie@users.noreply.github.com>
2026-04-11 11:49:02 +1000

    subir sponsors

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -13,10 +13,11 @@ export const SPONSORS: Sponsor[] = [
   { id: 5, name: "Art House", logo: "/images/Art_House_logo.png", url: "https://jean-lane-studios.square.site/" },
   { id: 6, name: "PCYC", logo: "/images/PCYC_logo.png", url: "https://www.pcyc.org.au" },
   { id: 7, name: "Speedfit QLD", logo: "/images/speedfit_logo.png", url: "https://speedfitspringwood.com/" },
-  { id: 8, name: "Australian Steak", logo: "/images/Australian_Steak_logo.png", url: "https://www.australiansteakco.com.au/" },
-  { id: 9, name: "Angelica RM cook", logo: "/images/Angelica_chef_LOGO.png", url: "https://www.instagram.com/angelica.r.m.chef/" },
-  { id: 10, name: "Latin Temple Beauty", logo: "/images/ltb.png", url: "https://latintemplebeauty.com.au/" },
-  { id: 11, name: "Roblading", logo: "/images/Roblading_logo.png", url: "https://www.roblading.com.au/#home" },
+  { id: 8, name: "Jessica Lule", logo: "/images/Jesica_lule_logo.png", url: "https://www.jesicalule.com/" },
+  { id: 9, name: "Australian Steak", logo: "/images/Australian_Steak_logo.png", url: "https://www.australiansteakco.com.au/" },
+  { id: 10, name: "Angelica RM cook", logo: "/images/Angelica_chef_LOGO.png", url: "https://www.instagram.com/angelica.r.m.chef/" },
+  { id: 11, name: "Latin Temple Beauty", logo: "/images/ltb.png", url: "https://latintemplebeauty.com.au/" },
+  { id: 12, name: "Roblading", logo: "/images/Roblading_logo.png", url: "https://www.roblading.com.au/#home" },
+  { id: 13, name: "Bienestar 360", logo: "/images/Bienestar360_logo.png", url: "https://latintemplebeauty.com.au/bienestar360-holisticwellness/" },
 
-  // Añade todos los que necesites
 ]
